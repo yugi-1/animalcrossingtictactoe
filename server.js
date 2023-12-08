32b7130e5b0314325c7855b1fdb59db412d93444
Yulissa Gil <yulissa.gil9759@stu.mtec.edu>
2023-12-07 18:19:12 -0700

    started server side

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require ('express');
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser');
+// const { query } = require('express');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('client'));
+
+
+
+app.listen(port, () => {
+    console.log(`this is ${port}`);
+});
